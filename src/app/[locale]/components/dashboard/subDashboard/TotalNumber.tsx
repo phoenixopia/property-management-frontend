586162199724b3e8f79f0690b5f3fb2a8132d55a
Daniel1234Daniel <dtekleyohannes@gmail.com>
2025-04-14 13:40:27 +0300

    localization for the main landing

--- src/app/[locale]/components/dashboard/subDashboard/TotalNumber.tsx
+++ src/app/[locale]/components/dashboard/subDashboard/TotalNumber.tsx
@@ -14,14 +14,14 @@ const TotalNumber = () => {
     <div className='flex flex-col lg:flex-row gap-4 w-full  overflow-auto max-h-[28rem] justify-evenly'>
         <div className='flex flex-row items-center dark:bg-gray-600 bg-white px-5 py-2 gap-4 rounded-md shadow-2xl'>
             <span className='text-sm font-bold text-gray-800 dark:text-gray-100'>
-                Total Number Of Properties:
+               {t2("Total Number Of Properties")}:
             </span>
 
             <p className='truncate max-w-[200px] text-sm'>125</p>
         </div>
         <div className='flex flex-row items-center dark:bg-gray-600 bg-white px-5 py-2 gap-4 rounded-md shadow-2xl'>
         <span className='text-sm font-bold text-gray-800  dark:text-gray-100'>
-                Total Number Of Tenants:
+                {t2("Total Number Of Tenants")}:
             </span>
 
             <p className='truncate max-w-[200px] text-sm'>12</p>
